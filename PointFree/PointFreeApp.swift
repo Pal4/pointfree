239ab32a47a52f2ae5a6c03bbccf2aec27ed45ad
Andrey Polunin <a.polunin@corp.vk.com>
2021-02-18 08:30:31 +0300

    Initial Commit

---
+++ PointFree/PointFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointFreeApp.swift
+//  PointFree
+//
+//  Created by a.polunin on 18.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct PointFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
